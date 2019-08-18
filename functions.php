0becb5b4e2c16f782e82c3482efd10bf6f24e4d4
Steve Lambert <67271+slambert@users.noreply.github.com>
2019-08-18 15:12:13 -0400

    fix: unexpected EOF

--- functions.php
+++ functions.php
@@ -170,3 +170,5 @@ function add_child_theme_textdomain() {
     load_child_theme_textdomain( 'understrap-child', get_stylesheet_directory() . '/languages' );
 }
 add_action( 'after_setup_theme', 'add_child_theme_textdomain' );
+
+}
